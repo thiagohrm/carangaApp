8948084806d8906e721292b894aabf3fd3a3fc5d
Thiago Mattos <tmattos1@ford.com>
2022-04-02 10:39:12 -0300

    Tarefa 3 - adicionado menu na main activity e codificado primeiras ações de navegação.

--- app/src/main/java/com/example/carangaapp/MainActivity.kt
+++ app/src/main/java/com/example/carangaapp/MainActivity.kt
@@ -3,25 +3,59 @@ package com.example.carangaapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.qualifiedName
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i(TAG, "onCreate()")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayoutMain)
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.open,
+            R.string.close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navView = findViewById<NavigationView>(R.id.navigationView)
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_carro_principal -> {
+                    Toast.makeText(applicationContext, it.title, Toast.LENGTH_LONG).show()
+                    closeMenu(drawerLayout)
+                }
+            }
+            true
+        }
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        Log.i(TAG, "onCreateOptionsMenu()")
-//        val inflater: MenuInflater = menuInflater
-//        inflater.inflate(R.menu.principal_menu, menu)
-//        return true
-//    }
+    private fun closeMenu(drawerLayout: DrawerLayout) {
+        Log.i(TAG, "closeMenu($drawerLayout)")
+        drawerLayout.closeDrawers()
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.i(TAG, "onOptionsItemSelected($item)")
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 
     override fun onStart() {
