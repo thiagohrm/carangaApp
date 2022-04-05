709ebcfd4fd80e653c1f2bf417cef5141adf23bf
Thiago Mattos <thiagohrm@gmail.com>
2022-04-05 18:27:31 -0300

    code refactor to a better naming convention

--- app/src/main/java/com/example/carangaapp/adapter/MainListItemAdapter.kt
+++ app/src/main/java/com/example/carangaapp/adapter/MainListItemAdapter.kt
@@ -7,38 +7,39 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.carangaapp.R
+import com.example.carangaapp.data.CarModel
 
-class PrincipalListItemAdapter(
-    val lista: List<PrincipalDataClass>,
-) : RecyclerView.Adapter<PrincipalListItemAdapter.ItemViewHolder>() {
+class MainListItemAdapter(
+    val carList: List<CarModel>,
+) : RecyclerView.Adapter<MainListItemAdapter.ItemViewHolder>() {
 
-    private val TAG = PrincipalListItemAdapter::class.qualifiedName
+    private val TAG = MainListItemAdapter::class.qualifiedName
 
     open class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val carroItem: TextView = view.findViewById<TextView>(R.id.tvItemCarro)
-        val placaItem: TextView = view.findViewById<TextView>(R.id.tvItemPlaca)
-        val marcaItem: TextView = view.findViewById<TextView>(R.id.tvItemMarca)
+        val carModelItem: TextView = view.findViewById<TextView>(R.id.tvItemModel)
+        val carPlateItem: TextView = view.findViewById<TextView>(R.id.tvItemPlate)
+        val carMakeItem: TextView = view.findViewById<TextView>(R.id.tvItemMake)
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         Log.i(TAG, "onCreateViewHolder( parent=$parent, viewType=$viewType )")
         val holderLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.principal_item, parent, false)
+            .inflate(R.layout.main_item, parent, false)
         return ItemViewHolder(holderLayout)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         Log.i(TAG, "onBindViewHolder( holder=$holder, position=$position )")
-        val item = lista[position]
-        holder.carroItem.text = item.modelo
-        holder.marcaItem.text = item.marca
-        holder.placaItem.text = item.placa
+        val item = carList[position]
+        holder.carModelItem.text = item.model
+        holder.carMakeItem.text = item.make
+        holder.carPlateItem.text = item.plate
     }
 
     override fun getItemCount(): Int {
         Log.i(TAG, "getItemCount()")
-        return lista.size
+        return carList.size
     }
 
 
---
+++ app/src/main/java/com/example/carangaapp/data/CarModel.kt
@@ -0,0 +1,8 @@
+package com.example.carangaapp.data
+
+data class CarModel(
+    val model : String,
+    val make : String,
+    val plate : String,
+    val year : Int
+)
